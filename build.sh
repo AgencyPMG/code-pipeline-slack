947af68909643a25b370075e869d11f48ce144fe
Pedro Coelho <punnie@ethereal.io>
2021-03-07 22:52:40 +0000

    Add extra steps to deploy script

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@
 # PROJECT_NAME=code-pipeline-slack
 # S3_BUCKET="${AWS_PROFILE}-cloudformation-artifacts-1"
 TARGET_DIRECTORY=./target
-ZIP_FILE=./lambda-$(date +%s).zip
+ZIP_FILE=./lambda.zip
 
 # if [[ -z ${AWS_PROFILE} ]]
 # then
@@ -27,6 +27,7 @@ echo "Installing dependencies into ${TARGET_DIRECTORY} ... "
 poetry run pip install -r <(poetry export -f requirements.txt) --target "${TARGET_DIRECTORY}"
 
 find ./code_pipeline_slack -type f -iname "*.py" -print0 | xargs -0 -I{} cp {} "${TARGET_DIRECTORY}"
+
 [ -f "${ZIP_FILE}" ] && rm -f "${ZIP_FILE}"
 
 pushd "${TARGET_DIRECTORY}"
@@ -35,9 +36,7 @@ popd
 
 [ -d "${TARGET_DIRECTORY}" ] && rm -rf "${TARGET_DIRECTORY}"
 
-# # upload zip package + template
-# echo "Uploading to s3://${S3_BUCKET} ${PROJECT_NAME}"
-# aws s3 mb --profile=${AWS_PROFILE}  --region=${AWS_REGION} s3://${S3_BUCKET}
-# aws cloudformation package --profile=${AWS_PROFILE} --region=${AWS_REGION} --template-file ./template.yml --s3-bucket ${S3_BUCKET} --s3-prefix ${PROJECT_NAME} --output-template-file packaged-template.yml
-# aws s3 --profile=${AWS_PROFILE} --region=${AWS_REGION} cp ./packaged-template.yml s3://${S3_BUCKET}/${PROJECT_NAME}/template.yml
+echo "Uploading to s3://${S3_BUCKET}/${PROJECT_NAME}"
+aws cloudformation package --template-file ./template.yml --s3-bucket ${S3_BUCKET} --s3-prefix ${PROJECT_NAME} --output-template-file packaged-template.yml
+aws s3 cp ./packaged-template.yml s3://${S3_BUCKET}/${PROJECT_NAME}/template.yml
 # aws cloudformation deploy --profile=${AWS_PROFILE} --region=${AWS_REGION} --template-file packaged-template.yml --stack-name ${PROJECT_NAME} --capabilities CAPABILITY_IAM
