ba8b996d4ddbffe2e5353ad18ebd07f314732dc8
Pedro Coelho <punnie@ethereal.io>
2021-03-07 00:21:52 +0000

    Classify slack helper

--- code_pipeline_slack/slack_helper.py
+++ code_pipeline_slack/slack_helper.py
@@ -1,133 +1,140 @@
-from slackclient import SlackClient
+from slack import WebClient
 import os
 import json
 import logging
-
-sc = SlackClient(os.getenv("SLACK_TOKEN"))
-sc_bot = SlackClient(os.getenv("SLACK_BOT_TOKEN"))
-SLACK_CHANNEL = os.getenv("SLACK_CHANNEL", "builds2")
-SLACK_CHANNEL_TYPE = os.getenv("SLACK_CHANNEL_TYPE", "public")
-SLACK_BOT_NAME = os.getenv("SLACK_BOT_NAME", "BuildBot")
-SLACK_BOT_ICON = os.getenv("SLACK_BOT_ICON", ":robot_face:")
 
 logger = logging.getLogger()
 
 LOGLEVEL = os.environ.get("LOGLEVEL", "WARNING").upper()
 logging.basicConfig(level=LOGLEVEL)
 
-CHANNEL_CACHE = {}
 
+class SlackHelper():
 
-def find_channel(name):
-    if name in CHANNEL_CACHE:
-        return CHANNEL_CACHE[name]
+    CHANNEL_CACHE = {}
+    MESSAGE_CACHE = {}
 
-    channel_type = (
-        "private_channel"
-        if SLACK_CHANNEL_TYPE.upper() == "PRIVATE"
-        else "public_channel"
-    )
+    def __init__(self, token, channel_name, channel_type, bot_name, bot_icon):
+        self.client = WebClient(token=token)
+        self.channel_name = channel_name
+        self.channel_type = channel_type
+        self.bot_name = bot_name
+        self.bot_icon = bot_icon
 
-    r = sc_bot.api_call("conversations.list", exclude_archived=1, types=channel_type)
-    if "error" in r:
-        logger.error(
-            "error getting channel with name '" + name + "': {}".format(r["error"])
-        )
-    else:
-        for ch in r["channels"]:
-            if ch["name"] == name:
-                CHANNEL_CACHE[name] = (ch["id"], ch["is_private"])
-                return CHANNEL_CACHE[name]
+    def find_channel(self, name):
+        if name in SlackHelper.CHANNEL_CACHE:
+            return SlackHelper.CHANNEL_CACHE[name]
 
-    return None, None
+        r = self.client.conversations_list(
+            exclude_archived=1, types=self.channel_type)
 
-def find_my_messages(ch_name, user_name=SLACK_BOT_NAME):
-    ch_id, is_private = find_channel(ch_name)
-    if not ch_id:
-        logger.error("error getting channel")
-        return
+        if "error" in r:
+            logger.error(
+                "error getting channel with name '" +
+                name + "': {}".format(r["error"])
+            )
+        else:
+            for ch in r["channels"]:
+                if ch["name"] == name:
+                    SlackHelper.CHANNEL_CACHE[name] = (
+                        ch["id"], ch["is_private"])
+                    return SlackHelper.CHANNEL_CACHE[name]
 
-    print("Channel id = ", ch_id)
-    msg = sc.api_call('conversations.history', channel=ch_id)
-    if 'error' in msg:
-        logger.error("error fetching msg for channel {}: {}".format(ch_id, msg['error']))
-    else:
-        for m in msg["messages"]:
-            if m.get("username") == user_name:
-                logger.debug("Found message: ", m)
-                yield m
+        return None, None
 
+    def find_message(self, ch, is_private):
+        if is_private:
+            return self.client.groups_history(channel=ch)
+        else:
+            return self.client.channels_history(channel=ch)
 
-MSG_CACHE = {}
+    def find_my_messages(self, ch_name, user_name=None):
+        ch_id, is_private = self.find_channel(ch_name)
 
+        if user_name is None:
+            user_name = self.bot_name
 
-def find_message_for_build(build_info):
-    cached = MSG_CACHE.get(build_info.executionId)
-    if cached:
-        return cached
+        if not ch_id:
+            logger.error("error getting channel")
+            return
 
-    for m in find_my_messages(SLACK_CHANNEL):
-        for att in msg_attachments(m):
-            if att.get("footer") == build_info.executionId:
-                MSG_CACHE[build_info.executionId] = m
-                return m
-    return None
+        print("Channel id = ", ch_id)
 
+        message = self.find_message(ch_id, is_private)
 
-def msg_attachments(message):
-    return message.get("attachments", [])
+        if "error" in message:
+            logger.error(
+                "error fetching message for channel {}: {}".format(
+                    ch_id, message["error"])
+            )
+        else:
+            for m in message["messages"]:
+                if m.get("username") == user_name:
+                    logger.debug("Found message: ", m)
+                    yield m
 
+    def find_message_for_build(self, build_info):
+        cached = SlackHelper.MESSAGE_CACHE.get(build_info.executionId)
+        if cached:
+            return cached
 
-def msg_fields(message):
-    for att in msg_attachments(message):
-        for f in att["fields"]:
-            yield f
+        for m in self.find_my_messages(self.channel_name):
+            for att in self.message_attachments(m):
+                if att.get("footer") == build_info.executionId:
+                    SlackHelper.MESSAGE_CACHE[build_info.executionId] = m
+                    return m
 
+        return None
 
-def post_build_msg(msg_builder):
-    ch_id, is_private = find_channel(SLACK_CHANNEL)
-    logger.debug("Channel id = ", ch_id)
+    def message_attachments(self, message):
+        return message.get("attachments", [])
 
-    # update existing message
-    if msg_builder.messageId:
+    def message_fields(self, message):
+        for att in self.message_attachments(message):
+            for f in att["fields"]:
+                yield f
 
-        msg = msg_builder.message()
-        logger.debug("Updating existing message")
-        r = update_msg(ch_id, msg_builder.messageId, msg)
-        logger.debug(json.dumps(r, indent=2))
-        if r["ok"]:
-            r["message"]["ts"] = r["ts"]
-            MSG_CACHE[msg_builder.buildInfo.executionId] = r["message"]
+    def post_build_message(self, message_builder):
+        ch_id, is_private = self.find_channel(self.channel_name)
+        logger.debug("Channel id = ", ch_id)
+
+        # update existing message
+        if message_builder.messageId:
+
+            message = message_builder.message()
+            logger.debug("Updating existing message")
+            r = self.update_message(ch_id, message_builder.messageId, message)
+            logger.debug(json.dumps(r, indent=2))
+            if r["ok"]:
+                r["message"]["ts"] = r["ts"]
+                SlackHelper.MESSAGE_CACHE[message_builder.buildInfo.executionId] = r["message"]
+            return r
+
+        logger.debug("New message")
+        r = self.send_message(ch_id, message_builder.message())
+
+        # if r['ok']:
+        # TODO: are we caching this ID?
+        # SlackHelper.MESSAGE_CACHE[msgBuilder.buildInfo.executionId] = r['ts']
+        # SlackHelper.CHANNEL_CACHE[self.channel_name] = (r['channel'], is_private)
+
         return r
 
-    logger.debug("New message")
-    r = send_msg(ch_id, msg_builder.message())
-    # if r['ok']:
-    # TODO: are we caching this ID?
-    # MSG_CACHE[msgBuilder.buildInfo.executionId] = r['ts']
-    # CHANNEL_CACHE[SLACK_CHANNEL] = (r['channel'], is_private)
+    def send_message(self, ch, attachments):
+        r = self.client.chat_postMessage(
+            channel=ch,
+            icon_emoji=self.bot_icon,
+            username=self.bot_name,
+            attachments=attachments,
+        )
+        return r
 
-    return r
-
-
-def send_msg(ch, attachments):
-    r = sc_bot.api_call(
-        "chat.postMessage",
-        channel=ch,
-        icon_emoji=SLACK_BOT_ICON,
-        username=SLACK_BOT_NAME,
-        attachments=attachments,
-    )
-    return r
-
-
-def update_msg(ch, ts, attachments):
-    r = sc_bot.api_call(
-        "chat.update",
-        channel=ch,
-        ts=ts,
-        icon_emoji=SLACK_BOT_ICON,
-        username=SLACK_BOT_NAME,
-        attachments=attachments,
-    )
-    return r
+    def update_message(self, ch, ts, attachments):
+        r = self.client.chat_update(
+            channel=ch,
+            ts=ts,
+            icon_emoji=self.bot_icon,
+            username=self.bot_name,
+            attachments=attachments,
+        )
+        return r
